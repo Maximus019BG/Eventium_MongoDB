9474fd3dc4380b0e91187acbd65c49f370f2e4f3
VexuBGM <emoniksp@gmail.com>
2024-01-08 01:16:01 +0200

    Creating the sidebar. Needs more styling

--- server/frontend/eventium/src/app/Components/navbar.tsx
+++ server/frontend/eventium/src/app/Components/navbar.tsx
@@ -10,10 +10,10 @@ const NavBar: React.FC = () => {
         <Image
           src={logo}
           alt="logo"
-          className='w-28 overflow-visible'
+          className='w-16 overflow-visible md:w-28 ml-2'
         />
        <center>
-        <div className='flex justify-center space-x-36 font-bold font-sans uppercase text-md'>
+        <div className='flex justify-center space-x-8 font-bold font-sans uppercase text-md md:space-x-36 md:text-sd sm:space-x-16'>
           <h1 className='nav'>Home</h1>
           <h1 className='nav'>Create</h1>
           <h1 className='nav'>Chat</h1>
@@ -23,7 +23,7 @@ const NavBar: React.FC = () => {
       <Image
         src={profile}
         alt="Profile"
-        className='w-12 mr-6'
+        className='w-12 mr-2 sm:mr-12'
       />
     </div>
   )
