09f8ce58f1eb699648b4fc9238e241700884554e
VexuBGM <emoniksp@gmail.com>
2024-01-09 16:12:04 +0200

    trying to style sideBar

--- server/frontend/eventium/src/app/Components/sidebar.tsx
+++ server/frontend/eventium/src/app/Components/sidebar.tsx
@@ -1,29 +1,37 @@
 'use client'
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const SideBar: React.FC = () => {
+  // const [isChecked, setChecked] = useState(false);
+
+
+  // const ColorChange = () => {
+  //   setChecked(!isChecked);
+  // };
+
   return (
-    <div className=' mt-[0.5px] fixed items-center bg-white h-screen shadow-sm shadow-slate-400 z-6 w-96 overflow-hidden text-3xl pt-4'>
+    <div className='flex flex-col items-center mt-[0.5px] fixed bg-white h-screen shadow-sm shadow-slate-400 z-6 w-60 overflow-hidden text-3xl pt-4'>
+      <h1 className='mb-4 text-4xl  text-black'>Filters</h1>
 
-      <h1 className=' mb-4 text-4xl left-2 font-bold text-black'>Filters</h1>
-
-      <form className=''>
-        <input
-        type="checkbox" 
-        id='Filter1'
-        className='' />
-        <label 
-        htmlFor="Filter1"
-        className='text-lg'
-        >
+      <form
+        className={`flex flex-row items-center`}
+        // className={`flex flex-row items-center ${isChecked ? 'border-2 border-green-500' : ''}`}
+      >
+        <label htmlFor="Filter1" className='text-xl px-3 cursor-pointer'>
           
           Filter 1
-        
+
         </label>
 
+        <input
+          type="checkbox"
+          id='Filter1'
+          className='w-6 h-6 m-2 accent-green-500 :hover:opasity-0.3 cursor-pointer'
+          // checked={isChecked}
+          // onChange={ColorChange}
+        />
       </form>
-      
+
 
     </div>
   );
