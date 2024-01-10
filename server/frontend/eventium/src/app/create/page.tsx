4a9d7cad015f19023ac98e7a113cf480ac60b312
Maximus019BG <maxralev@abv.bg>
2024-01-10 19:37:38 +0200

    Added create page not linked

---
+++ server/frontend/eventium/src/app/create/page.tsx
@@ -0,0 +1,62 @@
+'use client'
+import React from 'react'
+import NavBar from '../Components/navbar'
+
+
+const Create: React.FC = () => {
+    return (
+        <>
+          <NavBar />
+          <div className='w-5/6 h-5/6 shadow-sm shadow-slate-300 rounded-xl mx-36 my-36 p-24'>
+            <h1 className='my-10 font-bold'>Създай пост</h1>
+             <form > {/*onSubmit={handleSubmit}*/}
+              <div>
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  className="mt-1 p-2 w-full border-b-2 border-b-green-500 rounded-sm text-sm focus:outline-none"
+                //   value={title}
+                //   onChange={handleTitleChange}
+                  required
+                  placeholder='Заглавие'
+                />
+              </div>
+    
+              <div className="my-12">
+                <input
+                  type="text"
+                  id="description"
+                  name="description"
+                  className="mt-1 p-2 w-full border-b-2 border-b-green-500 rounded-sm text-sm focus:outline-none "
+                //   value={description}
+                //   onChange={handleDescriptionChange}
+                  required
+                  placeholder='Описание'
+                />
+              </div>
+    
+              <div className="my-12">
+                {/* Add photo input */}
+              </div>
+            
+            
+                    {/* NAPRAVI ADD PICTURE EMO I GO ISTRII */}
+
+
+              <div className="flex items-center justify-between mt-4 mb-3">
+                <input 
+                  type="submit"
+                  value="Създай пост" 
+                  className="bg-green-500 text-white font-semibold p-4 rounded-lg cursor-pointer text-bold hover:bg-green-600 text-md"
+                />
+              </div>
+            </form>
+           
+          </div>
+        </>
+      );
+    
+}
+
+export default Create;
