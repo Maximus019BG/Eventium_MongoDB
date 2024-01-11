f65d40e19f1a363f4a13e54f635a8eae1605f3b4
VexuBGM <emoniksp@gmail.com>
2024-01-11 19:14:02 +0200

    Some stylin to sidebar

--- server/frontend/eventium/src/app/Components/sidebar.tsx
+++ server/frontend/eventium/src/app/Components/sidebar.tsx
@@ -2,41 +2,35 @@
 import React, { useState } from 'react';
 
 const SideBar: React.FC = () => {
-  // const [isChecked, setChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
 
-
-  // const ColorChange = () => {
-  //   setChecked(!isChecked);
-  // };
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
 
   return (
-    <div className='flex flex-col items-center mt-[0.5px] fixed bg-white h-screen shadow-sm shadow-slate-400 z-6 w-60 overflow-hidden text-3xl pt-4'>
-      <h1 className='mb-4 text-4xl  text-black'>Filters</h1>
+    <div className='flex flex-col items-start mt-[0.5px] fixed bg-white h-screen shadow-sm shadow-slate-400 z-6 w-60 overflow-hidden text-3xl pt-4'>
+      <h1 className='mb-4 text-4xl text-black'>Filters</h1>
 
-      <form
-        className={`flex flex-row items-center`}
-        // className={`flex flex-row items-center ${isChecked ? 'border-2 border-green-500' : ''}`}
-      >
-        <label htmlFor="Filter1" className='text-xl px-3 cursor-pointer'>
-          
-          Filter 1
+      <form className={`flex flex-row items-start `}>
+        <div className={`border ${isChecked ? 'border-green-500' : 'border-white'} rounded-full p-1 flex items-center`}>
+          <label
+            htmlFor="Filter1"
+            className={`text-xl px-2 cursor-pointer`}
+          >
+            Filter 1
+          </label>
 
-        </label>
-
-        <input
-          type="checkbox"
-          id='Filter1'
-          className='w-6 h-6 m-2 accent-green-500 :hover:opasity-0.3 cursor-pointer'
-          // checked={isChecked}
-          // onChange={ColorChange}
-        />
+          <input
+            type="checkbox"
+            id='Filter1'
+            className='w-4 h-4 m-1 accent-green-500 cursor-pointer'
+            onChange={handleCheckboxChange}
+          />
+        </div>
       </form>
-
-
     </div>
   );
 }
 
 export default SideBar;
-
-
