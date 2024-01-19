708987897ee20ff9e446d3bda93ced4f1f7b475b
Maximus019BG <maxralev@abv.bg>
2024-01-19 12:35:45 +0200

    disign

--- server/frontend/eventium/src/app/Components/sidebar.tsx
+++ server/frontend/eventium/src/app/Components/sidebar.tsx
@@ -12,19 +12,19 @@ const SideBar: React.FC = () => {
   const filters = ['Filter 1', 'Filter 2', 'Filter 3', 'Filter 4']; // Add more filters as needed
 
   return (
-    <div className='flex flex-col items-start mt-[113px] fixed bg-white h-screen shadow-sm shadow-slate-400 z-6 w-60 overflow-hidden text-3xl pt-4'>
+    <div className='flex flex-col items-start mt-[113px] fixed bg-white h-screen shadow-sm shadow-slate-400 z-6 w-60 overflow-hidden text-2xl pt-4'>
       <h1 className='mb-4 text-4xl text-black'>Filters</h1>
 
       {filters.map((filter, index) => (
         <span
           key={index}
-          className={`border ${isSpanClicked[index] ? 'border-green-500 bg-green-100' : 'border-white'} p-1 rounded-full cursor-pointer m-3 relative`}
+          className={`border ${isSpanClicked[index] ? 'border-green-500 font-medium text-green-600 bg-green-100' : 'border-white font-normal'} pl-5 font  pr-3 py-2 lowercase rounded-full cursor-pointer m-3 relative`}
           onClick={() => handleSpanClick(index)}
         >
           {filter}
           {isSpanClicked[index] && (
-            <span className="p-2 px-2 cursor-pointer" onClick={() => handleSpanClick(index)}>
-              x
+            <span className="p-2 px-2 cursor-pointer font-bold text-2xl " onClick={() => handleSpanClick(index)}>
+              &nbsp; x
             </span>
           )}
         </span>
