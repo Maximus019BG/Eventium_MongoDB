166aa35201a9b3ea3729d898fa6b33c668cc17c6
Maximus019BG <maxralev@abv.bg>
2024-03-12 21:46:32 +0200

    Fix ?

--- server/EvF/next.config.mjs
+++
@@ -1,8 +0,0 @@
-import withVideos from 'next-videos'
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Your existing configuration options...
-}
-
-export default withVideos(nextConfig)
